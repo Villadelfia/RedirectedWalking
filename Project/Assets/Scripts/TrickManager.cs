b72ccaa2afbfdf579158a6608b1f9c818c689f7e
Randy Thiemann <randy.thiemann@student.uhasselt.be>
2014-04-18 16:57:31 +0200

    Add Stuffs

--- Project/Assets/Scripts/TrickManager.cs
+++ Project/Assets/Scripts/TrickManager.cs
@@ -1,48 +1,49 @@
-﻿using UnityEngine;
+﻿using System.ComponentModel;
+using UnityEngine;
 using System.Collections;
 
 public class TrickManager : MonoBehaviour {
-    public GameObject hallway;
-    public GameObject door1;
-    public GameObject door2;
-    public GameObject door3;
-    public GameObject door4;
-    public GameObject door5;
-    public GameObject door6;
-    public GameObject office;
-    GameObject officeInstance;
-    Transform button;
-    Transform blinds;
-    float timeout = 1.5f;
-
-    int ctr;
+    public GameObject Hallway;
+    public GameObject Door1;
+    public GameObject Door2;
+    public GameObject Door3;
+    public GameObject Door4;
+    public GameObject Office1;
+    public GameObject Office2;
+    public GameObject Office3;
+    public GameObject EndRoom;
+    GameObject _officeInstance;
+    Transform _button;
+    Transform _blinds;
+    float _timeout = 1.5f;
+    int _ctr;
 
     // Use this for initialization
     void Start() {
         Screen.showCursor = false;
-        ctr = 0;
-        officeInstance = (GameObject)Instantiate(office, new Vector3(-2.5f, 0f, -1.5f), Quaternion.identity);
-        button = officeInstance.transform.Find("button/button");
-        blinds = officeInstance.transform.Find("blinds");
-        StartCoroutine(RotateDoor(door1, 0.3f, false));
+        _ctr = 0;
+        _officeInstance = (GameObject)Instantiate(Office1, new Vector3(-2.5f, 0f, -1.5f), Quaternion.identity);
+        _button = _officeInstance.transform.Find("button/button");
+        _blinds = _officeInstance.transform.Find("blinds");
+        StartCoroutine(RotateDoor(Door1, 0.3f, false));
     }
 
     // Update is called once per frame
     void Update() {
-        timeout -= Time.deltaTime;
-        if(Input.GetButtonDown("Activate") && timeout <= 0f) {
-            timeout = 1.5f;
-            ++ctr;
-            switch(ctr) {
+        _timeout -= Time.deltaTime;
+        if(Input.GetButtonDown("Activate") && _timeout <= 0f) {
+            _timeout = 1.5f;
+            ++_ctr;
+            switch(_ctr) {
                 case 1: // Clicking button in room 1
                     StartCoroutine(MoveHallway1(0f, 0f, 1.6f));
                     StartCoroutine(DescendBlinds());
                     StartCoroutine(DepressButton());
                     break;
                 case 2: // Closing door of room 1
-                    StartCoroutine(RotateDoor(door1));
+                    StartCoroutine(RotateDoor(Door1));
                     StartCoroutine(RespawnRoom(0.6f));
-                    StartCoroutine(RotateDoor(door2, 0.6f, false));
+                    StartCoroutine(RotateDoor(Door2, 0.6f, false));
                     break;
                 case 3: // Clicking button in room 2
                     StartCoroutine(MoveHallway1(0f, 0f, 1.6f));
@@ -50,9 +51,9 @@ public class TrickManager : MonoBehaviour {
                     StartCoroutine(DepressButton());
                     break;
                 case 4: // Closing door of room 2
-                    StartCoroutine(RotateDoor(door2));
+                    StartCoroutine(RotateDoor(Door2));
                     StartCoroutine(RespawnRoom(0.6f));
-                    StartCoroutine(RotateDoor(door3, 0.6f, false));
+                    StartCoroutine(RotateDoor(Door3, 0.6f, false));
                     break;
                 case 5: // Clicking button in room 3
                     StartCoroutine(MoveHallway1(0f, 0f, 1.6f));
@@ -60,30 +61,9 @@ public class TrickManager : MonoBehaviour {
                     StartCoroutine(DepressButton());
                     break;
                 case 6: // Closing door of room 3
-                    StartCoroutine(RotateDoor(door3));
-                    StartCoroutine(RespawnRoom(0.6f, -90f, 0.55f, 0f, -2.425f));
-                    StartCoroutine(RotateDoor(door4, 0.6f, false));
-                    break;
-                case 7: // Clicking button in room 4
-                    StartCoroutine(MoveHallway1(-1.6f, 0f, 0f));
-                    StartCoroutine(DescendBlinds());
-                    StartCoroutine(DepressButton(0f, true));
-                    break;
-                case 8: // Closing door of room 4
-                    StartCoroutine(RotateDoor(door4));
-                    StartCoroutine(RespawnRoom(0.6f, -90f, 0.55f, 0f, -2.425f));
-                    StartCoroutine(RotateDoor(door5, 0.6f, false));
-                    break;
-                case 9: // Clicking button in room 5
-                    StartCoroutine(MoveHallway1(-1.6f, 0f, 0f));
-                    StartCoroutine(DescendBlinds());
-                    StartCoroutine(DepressButton(0f, true));
-                    break;
-                case 10: // Closing door of room 5
-                    StartCoroutine(RotateDoor(door5));
-                    StartCoroutine(RotateDoor(door6, 0.6f, false));
-                    break;
-                default:
+                    StartCoroutine(RotateDoor(Door3));
+                    StartCoroutine(RespawnRoom(0.6f, 0f, -1f, 0f, -1.6f));
+                    StartCoroutine(RotateDoor(Door4, 0.6f, false));
                     break;
             }
         }
@@ -92,7 +72,7 @@ public class TrickManager : MonoBehaviour {
     // Move the hallway 1.6m back.
     IEnumerator MoveHallway1(float x, float y, float z, float delay = 0) {
         yield return new WaitForSeconds(delay);
-        hallway.transform.Translate(x, y, z);
+        Hallway.transform.Translate(x, y, z);
         yield return true;
     }
 
@@ -115,40 +95,62 @@ public class TrickManager : MonoBehaviour {
 
     IEnumerator RespawnRoom(float delay = 0f, float rot = 0f, float x = -2.5f, float y = 0f, float z = -1.5f) {
         yield return new WaitForSeconds(delay);
-        if(office != null)
-            Destroy(officeInstance);
-        officeInstance = (GameObject)Instantiate(office, new Vector3(x, y, z), Quaternion.AngleAxis(rot, Vector3.up));
-        button = officeInstance.transform.Find("button/button");
-        blinds = officeInstance.transform.Find("blinds");
+        Destroy(_officeInstance);
+
+        switch (_ctr) {
+            case 2:
+                _officeInstance =
+                    (GameObject) Instantiate(Office2, new Vector3(x, y, z), Quaternion.AngleAxis(rot, Vector3.up));
+                _button = _officeInstance.transform.Find("button/button");
+                _blinds = _officeInstance.transform.Find("blinds");
+                break;
+            case 4:
+                _officeInstance =
+                    (GameObject) Instantiate(Office3, new Vector3(x, y, z), Quaternion.AngleAxis(rot, Vector3.up));
+                _button = _officeInstance.transform.Find("button/button");
+                _blinds = _officeInstance.transform.Find("blinds");
+                break;
+            case 6:
+                _officeInstance =
+                    (GameObject) Instantiate(EndRoom, new Vector3(x, y, z), Quaternion.AngleAxis(rot, Vector3.up));
+                _button = null;
+                _blinds = null;
+                break;
+            default:
+                _officeInstance =
+                    (GameObject) Instantiate(Office1, new Vector3(x, y, z), Quaternion.AngleAxis(rot, Vector3.up));
+                _button = _officeInstance.transform.Find("button/button");
+                _blinds = _officeInstance.transform.Find("blinds");
+                break;
+        }
         yield return true;
     }
 
     IEnumerator DepressButton(float delay = 0f, bool isRotated = false) {
         yield return new WaitForSeconds(delay);
-        Vector3 begin = button.transform.position;
+        Vector3 begin = _button.transform.position;
         Vector3 end = begin + (isRotated ? new Vector3(0.03f, 0f, 0f) : new Vector3(0f, 0f, -0.03f));
 
         for(float i = 0f; i < 0.25f; i += Time.deltaTime) {
-            button.transform.position = Vector3.Slerp(begin, end, i / 0.25f);
+            _button.transform.position = Vector3.Slerp(begin, end, i / 0.25f);
             yield return true;
         }
 
-        button.transform.position = end;
+        _button.transform.position = end;
         yield return true;
     }
 
     IEnumerator DescendBlinds(float delay = 0f) {
         yield return new WaitForSeconds(delay);
-        Vector3 begin = blinds.transform.position;
+        Vector3 begin = _blinds.transform.position;
         Vector3 end = begin - new Vector3(0f, 0.995f, 0f);
 
         for(float i = 0f; i < 1.5f; i += Time.deltaTime) {
-            blinds.transform.position = Vector3.Slerp(begin, end, i / 1.5f);
+            _blinds.transform.position = Vector3.Slerp(begin, end, i / 1.5f);
             yield return true;
         }
 
-        blinds.transform.position = end;
+        _blinds.transform.position = end;
         yield return true;
     }
-
 }
